3eb62c83f87e7c604e4d172b5c8f1dea9d714e9d
samhpr <samuel.hopper23@gmail.com>
2025-12-01 23:15:39 -0800

    adding comment for linters to ignore error f401 and giving reason for unused import

--- speechlab_diarization/backends/__init__.py
+++ speechlab_diarization/backends/__init__.py
@@ -1,14 +1,14 @@
 """
-Voice-type classification backends.
+voice-type classification backends
 
-Provides a unified interface for different voice-type classifiers (VTC 1.0, VTC 2.0, etc.)
+provides a unified interface for different voice-type classifiers (vtc 1.0, vtc 2.0, etc.)
 """
 
 from .base import VoiceTypeBackend, VoiceTypeSegment, get_backend, list_backends
 from .labels import CANONICAL_LABELS, normalize_label
 
-# Import backends to register them
-from . import vtc1  # noqa: F401 - registers VTC1Backend
+# import backends to register them
+from . import vtc1  # noqa: F401 - registers vtc1backend
 
 __all__ = [
     "VoiceTypeBackend",

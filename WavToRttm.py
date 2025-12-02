1e79843bf4c40f3429ba2ed5e6ae11b22042ca83
samhpr <samuel.hopper23@gmail.com>
2025-12-01 22:58:25 -0800

    adding summaries of files and how to run into readme

--- WavToRttm.py
+++ WavToRttm.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 
 # WARNING FROM SAM: this script has been refactored into the speechlab_diarization package
+# ^
+# ^
+# ^
+# ^
+# ^
+# ^
+# ^
+# ^
+# ^
+# ^
+# ^
+# ^
 
 from pathlib import Path
 
